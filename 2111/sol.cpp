3b63eedb37edbe5ebb50ab9c84e77daf855aec52
alex <alex.conev@gmail.com>
2016-03-27 00:00:19 +0200

    first portion

--- 2111/sol.cpp
+++ 2111/sol.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 int main() {
 
+	int br = 0;
+	char ch;
+
+	do
+	{
+		cin.get(ch);
+		if(ch == ' ') br++;
+	}while(ch!='.');
+
+	cout << br+1 << endl;
 
 	return 0;
 }
--- 2151/sol.cpp
+++ 2151/sol.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 int main() {
 
+	int br = 0;
+	char ch;
+
+	do
+	{
+		cin.get(ch);
+		if(ch == '\n') br++;
+	}while(ch != '.');
+
+	cout << br+1 << endl;
 
 	return 0;
 }
